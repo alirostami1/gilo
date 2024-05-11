4ae8abb179a6508207573a6dbe6477158f722586
Ali Rostami <ali.rostami@live.com>
2024-05-11 15:30:00 +0330

    entering raw mode

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/alirostami1/gilo
 
 go 1.22.0
+
+require golang.org/x/term v0.20.0
+
+require golang.org/x/sys v0.20.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "log"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+
+	"golang.org/x/term"
+	"log"
+)
 
 func main() {
-	log.Print("hello world")
+	prevState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		log.Fatalf("failed to switch to raw mode: %v", err)
+	}
+	defer term.Restore(int(os.Stdin.Fd()), prevState)
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		b, err := reader.ReadByte()
+		if err == io.EOF {
+			break
+		}
+		if b == byte('q') {
+			break
+		}
+		if b > 31 && b < 127 {
+			fmt.Printf("%d (%c)\r\n", b, b)
+		} else {
+			fmt.Printf("%d\r\n", b)
+		}
+	}
 }
